74ed03b849649eb3ef7f0b0d3a5eef67e3060237
kfharoun <kassandra.ferland@gmail.com>
2024-05-26 18:08:30 -0400

    functional crud

--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -1,17 +1,204 @@
-const Movie = require(`../models/movie`)
-const Actor = require(`../models/actor`)
-const Review = require(`../models/review`)
+const { Movie, Actor, Review } = require('../models')
+const mongoose = require('mongoose')
 
-// read
 const getAllMovies = async (req, res) => {
     try {
-        const movies = await Movie.find()
+        const movies = await Movie.aggregate([
+            {
+                $lookup: {
+                    from: 'actors',
+                    localField: '_id',
+                    foreignField: 'movie',
+                    as: 'actors'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'reviews',
+                    localField: '_id',
+                    foreignField: 'movie',
+                    as: 'reviews'
+                }
+            }
+        ])
+
         res.json(movies)
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+const getAllActors = async (req, res) => {
+    try {
+        const actors = await Actor.find({})
+        res.json(actors)
     } catch (error) {
         return res.status(500).send(error.message)
     }
 }
 
+const getAllReviews = async (req, res) => {
+    try {
+        const reviews = await Review.find({})
+        res.json(reviews)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+// https://www.mongodb.com/docs/manual/reference/operator/aggregation/lookup/
+// https://www.mongodb.com/docs/manual/reference/operator/aggregation/reverseArray/
+// https://www.mongodb.com/community/forums/t/sorting-with-mongoose-and-mongodb/122573
+const getAscendingReviews = async (req, res) => {
+    try {
+        const ascending = await Movie.aggregate([
+            {
+                $lookup: {
+                    from: 'reviews', 
+                    localField: '_id',
+                    foreignField: 'movie',
+                    as: 'reviews'
+                }
+            },
+            {
+                $addFields: {
+                    averageRating: { $avg: "$reviews.rating" } 
+                }
+            },
+            {
+                        $sort: { rating: 1 }
+                }
+        ])
+
+        res.json(ascending)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+const getDescendingReviews = async (req, res) => {
+    try {
+        const descending = await Movie.aggregate([
+            {
+                $lookup: {
+                    from: 'reviews', 
+                    localField: '_id',
+                    foreignField: 'movie',
+                    as: 'reviews'
+                }
+            },
+            {
+                $addFields: {
+                    averageRating: { $avg: "$reviews.rating" } 
+                }
+            },
+            {
+                $sort: { averageRating: -1 } 
+            },
+            {
+                $addFields: {
+                    reviews: {
+                        $reverseArray: "$reviews" 
+                    }
+                }
+            }
+        ])
+
+        res.json(descending)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+const sortMoviesByDate = async (req, res) => {
+    try {
+        const sortedMovies = await Movie.find({}).sort({ yearReleased: -1 })
+
+        res.json(sortedMovies)
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+// https://www.mongodb.com/docs/manual/reference/operator/aggregation/lookup/
+const getInfoById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const movie = await Movie.aggregate([
+            { $match: { _id: new mongoose.Types.ObjectId(id) } }, 
+            {
+                $lookup: {
+                    from: 'actors',
+                    localField: '_id',
+                    foreignField: 'movie',
+                    as: 'actors'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'reviews',
+                    localField: '_id',
+                    foreignField: 'movie',
+                    as: 'reviews'
+                }
+            }
+        ])
+
+        if (id) {
+            return res.json(movie)
+        }
+        return res.status(404).send(`Invalid Movie ID, please try again`)
+    } catch (error) {
+        return res.status(500).json({error: error.message})
+        }
+}
+
+const deleteMovie = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleted = await Movie.findByIdAndDelete(id)
+        if (deleted) {
+            return res.status(200).send("Movie deleted")
+        }
+        throw new Error("Movie not found")
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+const updateMovie = async (req, res) => {
+    try {
+        let { id } = req.params;
+        let movie = await Movie.findByIdAndUpdate(id, req.body, { new: true })
+        if (movie) {
+            return res.status(200).json(movie)
+        }
+        throw new Error("Movie not found")
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+const createMovie = async (req, res) => {
+    try {
+        const movie = await new Movie(req.body)
+        await movie.save()
+        return res.status(201).json({
+            movie
+        })
+    } catch (error) {
+        return res.status(500).json({error: error.message})
+    }
+}
 module.exports = {
-    getAllMovies
+    getAllMovies,
+    getAllActors,
+    getAllReviews, 
+    getAscendingReviews, 
+    getDescendingReviews,
+    sortMoviesByDate, 
+    getInfoById, 
+    deleteMovie, 
+    updateMovie, 
+    createMovie
 }
--- server.js
+++ server.js
@@ -1,21 +1,48 @@
-const express = require(`express`)
-const db = require(`./db`)
-const movieControllers = require(`./controllers/movieControllers`)
+const express = require('express')
+const db = require('./db')
+const movieControllers = require('./controllers/movieControllers')
 const bodyParser = require('body-parser')
-const logger = require(`morgan`)
+const logger = require('morgan')
 
 const PORT = process.env.PORT || 3001
 
 const app = express()
-app.use(logger(`dev`))
+app.use(logger('dev'))
 app.use(bodyParser.json())
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}`)
+})
 
 app.get('/', (req, res) => {
   res.send('This is our root page!')
 })
 
-app.listen(PORT, () => {
-    console.log(`Express server listening on port ${PORT}`)
-  })
+app.get('/movies', movieControllers.getAllMovies)
+
+app.get('/reviews', movieControllers.getAllReviews)
+
+app.get(`/actors`, movieControllers.getAllActors)
+
+//  I want to sort my reviews by ascending or descending order
+app.get(`/reviews/ascending`, movieControllers.getAscendingReviews)
+
+app.get(`/reviews/descending`, movieControllers.getDescendingReviews)
+
+// I want to sort my movies by newest or oldest
+
+app.get(`/movie/date`, movieControllers.sortMoviesByDate)
+
+// I want an error message to display if I search for an Actor, Movie, or Review with an incorrect ID
+
+app.get(`/movie/:id`, movieControllers.getInfoById)
+
+// create
+app.post(`/movies`, movieControllers.createMovie)
+ 
+// update
+app.put(`/movie/:id`, movieControllers.updateMovie)
+
+// delete 
+
+app.delete('/movie/:id', movieControllers.deleteMovie)
